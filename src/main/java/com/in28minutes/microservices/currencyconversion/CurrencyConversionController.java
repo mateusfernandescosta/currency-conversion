d93f0cef7ccffd28233e717328b55932bdfcc5b8
mateusfcosta <mateus.costa@mediaportal.com.br>
2018-10-28 16:43:23 -0200

    FEAT: Add Seleuth - Same RequestId

--- src/main/java/com/in28minutes/microservices/currencyconversion/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversion/CurrencyConversionController.java
@@ -6,6 +6,8 @@ import java.util.Formatter.BigDecimalLayoutForm;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
 	
@@ -38,6 +42,8 @@ public class CurrencyConversionController {
 			@PathVariable String to, @PathVariable BigDecimal quantity) {
 		
 		CurrencyConversion response = proxy.retrieveExchangeValue(from, to);
+		
+		logger.info("{}", response);
 		
 		return new CurrencyConversion(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
